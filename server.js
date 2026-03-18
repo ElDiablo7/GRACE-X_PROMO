98a18722cbc709f26d6be1bd23c65d2eeca50c7f
ElDiablo7 <zacc78@gmail.com>
2026-03-18 13:55:03 +0000

    fix: add start script and 0.0.0.0 binding for Render deployment

--- server.js
+++ server.js
@@ -193,6 +193,6 @@ app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
-    console.log(`GRACE-X Web Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`GRACE-X Web Server running on http://0.0.0.0:${PORT}`);
 });
